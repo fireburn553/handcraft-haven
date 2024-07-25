a103bc102cbdecbdf977f5e3a6e2e9ab670183d4
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-07-25 13:38:24 +0800

    added some configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-import { env } from "process";
-
-//** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
